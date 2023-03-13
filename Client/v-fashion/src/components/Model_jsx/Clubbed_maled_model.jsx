c489fca486477195feff7151e8502352b1e8a968
Soham <mastpanchalsoham@gmail.com>
2023-03-13 17:05:27 +0530

    Added model body asset picker interface

--- Client/v-fashion/src/components/Model_jsx/Clubbed_maled_model.jsx
+++ Client/v-fashion/src/components/Model_jsx/Clubbed_maled_model.jsx
@@ -5,17 +5,18 @@ Command: npx gltfjsx@6.1.4 clubbed_maled_model.glb
 
 import React, { useRef } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
+import { useModelCustomization } from '../../contexts/ModelCustomizationContext'
 
 export function Clubbed_male_model(props) {
   const group = useRef()
   const { nodes, materials, animations } = useGLTF('assets/models/clubbed_maled_model.glb')
   const { actions } = useAnimations(animations, group)
 
-  const hairVersion = 1;
-  const specsVersion = 1;
-  const upperVersion = 0;
-  const lowerVersion = 1;
-  const footwearVersion = 1;
+  const { hairStyle, setHairStyle } = useModelCustomization();
+  const { specs, setSpecs } = useModelCustomization();
+  const { topOutfit, setTopOutfit } = useModelCustomization();
+  const { bottomOutfit, setBottomOutfit } = useModelCustomization();
+  const { footwear, setFootwear } = useModelCustomization();
 
   return (
     <group ref={group} {...props} dispose={null}>
@@ -25,15 +26,15 @@ export function Clubbed_male_model(props) {
           <group name="Armature001" position={[0, -2.5, 0]} scale={[2.5, 2.5, 2.5]} castShadow receiveShadow>
             <primitive object={nodes.Hips} />
             <skinnedMesh name="Wolf3D_Body001" geometry={nodes.Wolf3D_Body001.geometry} material={materials['Wolf3D_Body.001']} skeleton={nodes.Wolf3D_Body001.skeleton} />
-            {specsVersion == 0 && <skinnedMesh name="Wolf3D_Glasses002" geometry={nodes.Wolf3D_Glasses002.geometry} material={materials['Wolf3D_Glasses.002']} skeleton={nodes.Wolf3D_Glasses002.skeleton} castShadow/>}
-            {hairVersion == 0 && <skinnedMesh name="Wolf3D_Hair001" geometry={nodes.Wolf3D_Hair001.geometry} material={materials['Wolf3D_Hair.001']} skeleton={nodes.Wolf3D_Hair001.skeleton} castShadow/>}
-            {hairVersion == 1 && <skinnedMesh name="Wolf3D_Hair003" geometry={nodes.Wolf3D_Hair003.geometry} material={materials['Wolf3D_Hair.003']} skeleton={nodes.Wolf3D_Hair003.skeleton} castShadow/>}
-            {lowerVersion == 0 && <skinnedMesh name="Wolf3D_Outfit_Bottom001" geometry={nodes.Wolf3D_Outfit_Bottom001.geometry} material={materials['Wolf3D_Outfit_Bottom.001']} skeleton={nodes.Wolf3D_Outfit_Bottom001.skeleton} castShadow/>}
-            {lowerVersion == 1 && <skinnedMesh name="Wolf3D_Outfit_Bottom003" geometry={nodes.Wolf3D_Outfit_Bottom003.geometry} material={materials['Wolf3D_Outfit_Bottom.003']} skeleton={nodes.Wolf3D_Outfit_Bottom003.skeleton} castShadow/>}
-            {footwearVersion == 0 && <skinnedMesh name="Wolf3D_Outfit_Footwear001" geometry={nodes.Wolf3D_Outfit_Footwear001.geometry} material={materials['Wolf3D_Outfit_Footwear.001']} skeleton={nodes.Wolf3D_Outfit_Footwear001.skeleton} castShadow/>}
-            {footwearVersion == 1 && <skinnedMesh name="Wolf3D_Outfit_Footwear003" geometry={nodes.Wolf3D_Outfit_Footwear003.geometry} material={materials['Wolf3D_Outfit_Footwear.003']} skeleton={nodes.Wolf3D_Outfit_Footwear003.skeleton} castShadow/>}
-            {upperVersion == 0 && <skinnedMesh name="Wolf3D_Outfit_Top001" geometry={nodes.Wolf3D_Outfit_Top001.geometry} material={materials['Wolf3D_Outfit_Top.001']} skeleton={nodes.Wolf3D_Outfit_Top001.skeleton} castShadow/>}
-            {upperVersion == 1 && <skinnedMesh name="Wolf3D_Outfit_Top003" geometry={nodes.Wolf3D_Outfit_Top003.geometry} material={materials['Wolf3D_Outfit_Top.003']} skeleton={nodes.Wolf3D_Outfit_Top003.skeleton} castShadow/>}
+            {specs == 0 && <skinnedMesh name="Wolf3D_Glasses002" geometry={nodes.Wolf3D_Glasses002.geometry} material={materials['Wolf3D_Glasses.002']} skeleton={nodes.Wolf3D_Glasses002.skeleton} castShadow/>}
+            {hairStyle == 0 && <skinnedMesh name="Wolf3D_Hair001" geometry={nodes.Wolf3D_Hair001.geometry} material={materials['Wolf3D_Hair.001']} skeleton={nodes.Wolf3D_Hair001.skeleton} castShadow/>}
+            {hairStyle == 1 && <skinnedMesh name="Wolf3D_Hair003" geometry={nodes.Wolf3D_Hair003.geometry} material={materials['Wolf3D_Hair.003']} skeleton={nodes.Wolf3D_Hair003.skeleton} castShadow/>}
+            {bottomOutfit == 0 && <skinnedMesh name="Wolf3D_Outfit_Bottom001" geometry={nodes.Wolf3D_Outfit_Bottom001.geometry} material={materials['Wolf3D_Outfit_Bottom.001']} skeleton={nodes.Wolf3D_Outfit_Bottom001.skeleton} castShadow/>}
+            {bottomOutfit == 1 && <skinnedMesh name="Wolf3D_Outfit_Bottom003" geometry={nodes.Wolf3D_Outfit_Bottom003.geometry} material={materials['Wolf3D_Outfit_Bottom.003']} skeleton={nodes.Wolf3D_Outfit_Bottom003.skeleton} castShadow/>}
+            {footwear == 0 && <skinnedMesh name="Wolf3D_Outfit_Footwear001" geometry={nodes.Wolf3D_Outfit_Footwear001.geometry} material={materials['Wolf3D_Outfit_Footwear.001']} skeleton={nodes.Wolf3D_Outfit_Footwear001.skeleton} castShadow/>}
+            {footwear == 1 && <skinnedMesh name="Wolf3D_Outfit_Footwear003" geometry={nodes.Wolf3D_Outfit_Footwear003.geometry} material={materials['Wolf3D_Outfit_Footwear.003']} skeleton={nodes.Wolf3D_Outfit_Footwear003.skeleton} castShadow/>}
+            {topOutfit == 0 && <skinnedMesh name="Wolf3D_Outfit_Top001" geometry={nodes.Wolf3D_Outfit_Top001.geometry} material={materials['Wolf3D_Outfit_Top.001']} skeleton={nodes.Wolf3D_Outfit_Top001.skeleton} castShadow/>}
+            {topOutfit == 1 && <skinnedMesh name="Wolf3D_Outfit_Top003" geometry={nodes.Wolf3D_Outfit_Top003.geometry} material={materials['Wolf3D_Outfit_Top.003']} skeleton={nodes.Wolf3D_Outfit_Top003.skeleton} castShadow/>}
             <skinnedMesh name="EyeLeft001" geometry={nodes.EyeLeft001.geometry} material={materials['Wolf3D_Eye.001']} skeleton={nodes.EyeLeft001.skeleton} morphTargetDictionary={nodes.EyeLeft001.morphTargetDictionary} morphTargetInfluences={nodes.EyeLeft001.morphTargetInfluences} castShadow/>
             <skinnedMesh name="EyeRight001" geometry={nodes.EyeRight001.geometry} material={materials['Wolf3D_Eye.001']} skeleton={nodes.EyeRight001.skeleton} morphTargetDictionary={nodes.EyeRight001.morphTargetDictionary} morphTargetInfluences={nodes.EyeRight001.morphTargetInfluences} castShadow/>
             <skinnedMesh name="Wolf3D_Head001" geometry={nodes.Wolf3D_Head001.geometry} material={materials['Wolf3D_Skin.001']} skeleton={nodes.Wolf3D_Head001.skeleton} morphTargetDictionary={nodes.Wolf3D_Head001.morphTargetDictionary} morphTargetInfluences={nodes.Wolf3D_Head001.morphTargetInfluences} castShadow/>
