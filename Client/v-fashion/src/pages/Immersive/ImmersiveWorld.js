2fd586a2de5f0e55086bed694725dd901c928eab
Soham <mastpanchalsoham@gmail.com>
2023-03-23 11:45:50 +0530

    Adjusted the position of the player in the immersive world

--- Client/v-fashion/src/pages/Immersive/ImmersiveWorld.js
+++ Client/v-fashion/src/pages/Immersive/ImmersiveWorld.js
@@ -1,22 +1,64 @@
-import React, {useRef} from "react";
-import { Canvas, GridHelperProps } from "@react-three/fiber";
-import { CameraShake, Environment, OrbitControls, useHelper } from "@react-three/drei";
+import React, {useRef, useEffect, Suspense} from "react";
+import { Canvas, GridHelperProps, useThree } from "@react-three/fiber";
+import { CameraShake, Environment, OrbitControls, Sparkles, useHelper } from "@react-three/drei";
 import { Fashion_show_hall } from "../../components/Model_jsx/Fashion_show_hall";
-import { SpotLightHelper } from "three";
+import { Camera, SpotLightHelper } from "three";
 import { VRButton, XR } from "@react-three/xr"
+import { useXR, useFrame } from '@react-three/xr'
+import VRCameracontrols from "../../components/Controls/VRCameracontrols";
+import ReactMediaQuery from "../../utils/ReactMediaQuery";
+
+const MovePlayer = () => {
+
+  const { isPresenting, player } = useXR();
+
+  useEffect(() => {
+    if (isPresenting) {
+      player.position.x = -5
+      player.position.y = 0.25
+      player.position.z = 15
+    }
+  }, [isPresenting])
+}
 
 const ImmersiveWorld = () => {
 
-  // const light = useRef();
-  // useHelper(light, SpotLightHelper, 'red')
+  const isPhone = ReactMediaQuery('(max-width: 400px)');
+
+  
+
+
+    const XRVButtonStyles = {
+      zIndex: '2',
+      backgroundColor:'greenyellow',
+      borderStyle:'solid',
+      position:'fixed',
+      width:'6rem',
+      heigth:'1rem',
+      left: isPhone ? '40vw': '47vw',
+      top:'85vh',
+      
+    
+
+    }
+    
+    
   
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <VRButton />
+      <VRButton
+        style={XRVButtonStyles} 
+      />
       <Canvas>
-        <XR>
-          <camera position={[-1, 0.5, 2]} />
-          <Fashion_show_hall />
+        <XR
+          onSessionStart={() => {
+            new Camera.prototype.position.set(-1, 2, 2);
+          }}
+        >
+          <MovePlayer />
+          <Suspense fallback={null} >
+            <Fashion_show_hall />
+          </Suspense>
           <OrbitControls />
           <Environment files='./assets/HDRIs/hansaplatz_4k.hdr' background blur={0.075} />
           <ambientLight args={[1]} />
